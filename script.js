b4a430e1a1b246347114672968d2c1da01d53fe8
Livioamarante7 <livioamarante7@gmail.com>
2022-06-17 11:54:10 -0300

    verificacaodeidade

---
+++ script.js
@@ -0,0 +1,49 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            gênero = 'homem'
+            if (idade >=0 && idade <10) {
+                //criança
+                img.setAttribute('src', 'bebem.jpeg' )
+            }else if (idade <21){
+                //jovem
+                img.setAttribute('src', 'jovemm.jpeg' )
+            }else if (idade <50){
+                //adulto
+                img.setAttribute('src', 'adultom.jpeg' )
+            }else{
+                //idoso
+                img.setAttribute('src', 'idosom.jpeg' )
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'mulher'
+            if (idade >=0 && idade <10) {
+                //criança
+                img.setAttribute('src', 'bebef.jpeg' )
+            } else if (idade <21) {
+                //jovem
+                img.setAttribute('src', 'jovemf.jpeg' )
+            } else if (idade <50) {
+                //adulto
+                img.setAttribute('src', 'adultof.jpeg' )
+            } else {
+                //idoso
+                img.setAttribute('src', 'idosof.jpeg' )
+            }        
+        } 
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}    
